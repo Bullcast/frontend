bec77c6d317094d939338207c25abb62418886d1
HoangDucBach <hoangbach0985@gmail.com>
2025-02-02 07:26:26 +0700

    feat: user prediction card

--- src/app/(dashboard)/prediction/_components/UserPredictionCard.tsx
+++ src/app/(dashboard)/prediction/_components/UserPredictionCard.tsx
@@ -1,14 +1,49 @@
+import { Flex, Box, Text, Heading, Image as ChakraImage } from "@chakra-ui/react";
+
 type User = {
     name: string;
     avatar: string;
     rank: number;
 };
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+    ishigher?: any;
+}
 export const UserPredictionCard: React.FC<Props> = (props) => {
-  return (
-	<div {...props}>
-	  {/* Add your component content here */}
-	</div>
-  );
+    const { ishigher = undefined } = props;
+
+    const user: User = {
+        name: "John Doe",
+        avatar: "https://miro.medium.com/v2/resize:fit:740/1*ooOH6jo8I0ns0J-BE0SAow.jpeg",
+        rank: [1, 2, 3][Math.floor(Math.random() * 3)],
+    };
+
+    return (
+        <div
+            {...props}
+            style={{
+                transform: ishigher ? "translateY(-64px)" : "translateY(0)",
+            }}
+        // translateY={isHigher ? "-64" : "0"}
+        >
+            <Flex align="center" gap="4" direction={"column"}>
+                <Flex w={"full"} align="center" gap="1" direction={"row"} justify={"end"}>
+                    <Box w={"6"} h={"1"} borderRadius={"full"} bg={'fg'} />
+                    <Text fontSize={"4xl"} fontWeight={"bold"}>
+                        {user.rank}
+                    </Text>
+                </Flex>
+                <ChakraImage
+                    borderRadius={"full"}
+                    w={"16"}
+                    h={"16"}
+                    src={user.avatar}
+                    alt={user.name}
+                />
+                <Heading size="2xl" fontWeight={"bold"}>
+                    {user.name}
+                </Heading>
+            </Flex>
+        </div>
+    );
 }
--- src/app/(dashboard)/prediction/page.tsx
+++ src/app/(dashboard)/prediction/page.tsx
@@ -1,13 +1,18 @@
 import { PageHeader } from "@/components/molecules";
 import { Container, Flex } from "@chakra-ui/react";
+import { UserPredictionCard } from "./_components/UserPredictionCard";
 
 
 export default function Prediction() {
   return (
-    <Flex direction={"column"} flex="1">
+    <Flex direction={"column"} flex="1" gap={"6"}>
       <PageHeader title="Prediction" />
       <main>
-        
+        <Flex direction={"row"} flex="1" justify={"space-between"}>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <UserPredictionCard key={index} ishigher={index === 1 ? "true" : undefined} />
+          ))}
+        </Flex>
       </main>
     </Flex>
   );
