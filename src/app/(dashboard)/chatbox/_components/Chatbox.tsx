aebb7ec2e484975acd8980581520fa4b46616af5
HoangDucBach <hoangbach0985@gmail.com>
2025-02-06 10:45:05 +0700

    add: intro and upgrade chatbox

--- src/app/(dashboard)/chatbox/_components/Chatbox.tsx
+++ src/app/(dashboard)/chatbox/_components/Chatbox.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Box, Flex, FlexProps, Group, HStack, Icon, IconButton, Input, Link, Text } from '@chakra-ui/react';
+import { Box, Flex, FlexProps, Group, HStack, Icon, IconButton, Input, Link, Span, Text } from '@chakra-ui/react';
 import { useChat, Message } from 'ai/react';
 import React from 'react';
 import { BiPaperPlane } from 'react-icons/bi';
 import { FaRegImage } from 'react-icons/fa';
 import { IoCopyOutline, IoReload } from 'react-icons/io5';
+import { MdOutlineLocalFireDepartment, MdOutlineRocket } from 'react-icons/md';
 import Markdown from 'react-markdown'
 import RemarkGfm from 'remark-gfm';
 import RehypeRaw from 'rehype-raw';
@@ -19,8 +20,9 @@ import { APP_NAME } from '@/utils/constants';
 
 interface Props extends FlexProps { }
 export const Chatbox: React.FC<Props> = (props) => {
-    const { messages, input, handleSubmit, handleInputChange, isLoading, error, reload } =
+    const { messages, input, handleSubmit, handleInputChange, isLoading, error, reload, setMessages } =
         useChat();
+    const messagesEndRef = React.useRef<HTMLDivElement>(null);
 
     const InputImage = React.memo(() => (
         <Icon as={FaRegImage} color={"fg.muted"} size={"md"} />
@@ -39,9 +41,19 @@ export const Chatbox: React.FC<Props> = (props) => {
     ));
     NoticeMessages.displayName = "NoticeMessages";
 
+    React.useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
     return (
         <Flex {...props} w={'full'} h={'full'} direction={'column'} gap={'6'} overflow={'hidden'}>
             <Flex direction={'column'} gap={'4'} flex={1} overflow={"scroll"}>
+                {messages.length === 0 && (
+                    <MessageIntro
+                        handleInputChange={handleInputChange}
+                        setMessages={setMessages}
+                    />
+                )}
                 {messages.map(message => (
                     message.role === 'user' ? (
                         <MessageUser key={message.id} message={message} />
@@ -49,6 +61,7 @@ export const Chatbox: React.FC<Props> = (props) => {
                         <MessageBot key={message.id} message={message} error={error} funcs={{ reload }} />
                     )
                 ))}
+                <div ref={messagesEndRef} />
             </Flex>
             <Flex direction={'column'} gap={'4'}>
                 <form onSubmit={handleSubmit}>
@@ -261,5 +274,94 @@ const MessageAttachment: React.FC<MessageAttachmentProps> = (props) => {
                 {attachment.url}
             </Text>
         </Box>
+    );
+}
+
+
+interface MessageIntroProps extends React.HTMLAttributes<HTMLDivElement> {
+    handleInputChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+    setMessages?: React.Dispatch<React.SetStateAction<Message[]>>;
+}
+export const MessageIntro: React.FC<MessageIntroProps> = (props) => {
+    const { handleInputChange } = props;
+
+    const MessageDemo = (props: { demoContent: string, title: string, icon: React.ReactElement }) => {
+        const { demoContent, title, } = props;
+
+        return (
+            <Box
+                w={'fit-content'}
+                p={'4'}
+                bg={'bg.muted'}
+                borderRadius={'md'}
+                borderLeft={'2px'}
+                borderColor={'primary'}
+                _hover={{
+                    cursor: 'pointer',
+                    scale: '105%',
+                    transition: 'all 0.3s',
+                }}
+                onClick={() => {
+                    handleInputChange && handleInputChange({ target: { value: demoContent } } as any);
+                }}
+                {...props}
+            >
+                <Flex direction={'column'} gap={'2'} align={'center'}>
+                    {props.icon}
+                    <Text fontSize={'lg'} fontWeight={'bold'} textAlign={'center'}>
+                        {title}
+                    </Text>
+                    <Text fontSize={'md'} color={'fg.muted'} textAlign={'center'}>
+                        {demoContent}
+                    </Text>
+                </Flex>
+            </Box>
+        );
+    }
+
+    return (
+        <Flex direction={'column'} gap={'4'} justify={'center'} align={'center'} {...props}>
+            <Icon
+                w={'16'}
+                h={'16'}
+                borderRadius={'sm'}
+                bg={'primary.3'}
+                p={'2'}
+            >
+                <Logo />
+            </Icon>
+            <Text fontSize={'4xl'} fontWeight={'bold'} textAlign={'center'}>
+                Welcome to <Span color={'primary'}>{APP_NAME}</Span> AI Chatbox!
+            </Text>
+            <Text fontSize={'md'} color={'fg.muted'} textAlign={'center'}>
+                This is the hint message to help users know how to use!
+            </Text>
+            <Flex direction={'row'} gap={'4'} justify={'center'} align={'center'}>
+                <MessageDemo
+                    title={'Get Started'}
+                    demoContent={'What is BullCast?'}
+                    icon={
+                        <Icon
+                            size={'md'}
+                            color={'fg.muted'}
+                        >
+                            <MdOutlineRocket />
+                        </Icon>
+                    }
+                />
+                <MessageDemo
+                    title={'Next'}
+                    demoContent={'How to use BullCast?'}
+                    icon={
+                        <Icon
+                            size={'md'}
+                            color={'fg.muted'}
+                        >
+                            <MdOutlineLocalFireDepartment />
+                        </Icon>
+                    }
+                />
+            </Flex>
+        </Flex>
     );
 }
