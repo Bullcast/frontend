d6ca09feda926078d8e562734e1f5bf20f0e1c69
HoangDucBach <hoangbach0985@gmail.com>
2025-02-07 20:26:17 +0700

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-empty-interface": "off",
+      
+      "@typescript-eslint/ban-types": ["error", { "types": { "{}": false } }],
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
